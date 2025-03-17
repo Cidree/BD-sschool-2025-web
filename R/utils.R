f07b95e07f84b4a60b7a036abcf64133498d31b2
Cidre-Gesplan <acidgon@gesplan.es>
2025-03-17 22:25:28 +0000

    add base tables

--- R/utils.R
+++ R/utils.R
@@ -57,3 +57,127 @@ teacher_card <- function(name, photo.dir, desc.dir, linkedin) {
     )
   )
 }
+
+
+agenda_table <- function(sheet, subtitle = "*Día 1: Herramientas geoinformáticas*") {
+  dat <- read_xlsx("00_assets/data/BD-jornadas-202506.xlsx", sheet = sheet) |>
+    mutate(
+      Comienzo = as.character(Comienzo) |> str_sub(start = 12, end = 16),
+      Fin      = as.character(Fin) |> str_sub(start = 12, end = 16)
+    ) |>
+    fill(
+      Comienzo:Fin,
+      .direction = "down"
+    ) |>
+    mutate(Horario  = paste(Comienzo, "-", Fin)) |>
+    select(Horario, Título:Coordinador) |>
+    group_by(Horario) |>
+    mutate(id = cur_group_id()) |>
+    ungroup()
+
+
+  dat |>
+    select(-id) |>
+    gt(
+      groupname_col       = "Horario",
+      row_group_as_column = TRUE
+    ) |>
+    tab_header(
+      title = "Agenda",
+      subtitle = md(subtitle)
+    ) |>
+    tab_options(
+      # Alineación y fuente
+      heading.align = "center",
+      table.font.size = px(14),
+      data_row.padding = px(5),
+      row_group.padding = px(10),
+      # Bordes de la tabla
+      table.border.top.style = "solid",
+      table.border.top.width = px(2),
+      table.border.top.color = "black",
+      table.border.bottom.style = "solid",
+      table.border.bottom.width = px(2),
+      table.border.bottom.color = "black",
+      # Bordes en los grupos de filas
+      row_group.border.top.style = "solid",
+      row_group.border.top.width = px(1),
+      row_group.border.top.color = "gray",
+      row_group.border.bottom.style = "solid",
+      row_group.border.bottom.width = px(1),
+      row_group.border.bottom.color = "gray"
+    ) |>
+    ## title style
+    tab_style(
+      style = list(
+        cell_fill(color = "#002500"),
+        cell_text(color = "#e1e3e1")
+      ),
+      locations = cells_title(groups = c("title", "subtitle"))
+    ) |>
+    ## horario column (even rows)
+    tab_style(
+      style = list(
+        cell_text(color = "#FFCF99", weight = "bold"),  # Peach title text
+        cell_fill(color = "#8C6239")  # Brown background
+      ),
+      locations = cells_row_groups()
+    ) |>
+    ## body rows (even rows)
+    tab_style(
+      style = list(
+        cell_fill(color = "#f9ffe9"),  # Peach title text
+        cell_text(color = "#002500")  # Brown background
+      ),
+      locations = cells_body(
+        rows = which(dat$id %% 2 == 0)
+      )
+    ) |>
+    ## body rows (uneven rows)
+    tab_style(
+      style = list(
+        cell_fill(color = "#e1e3e1"),  # Peach title text
+        cell_text(color = "#002500")  # Brown background
+      ),
+      locations = cells_body(
+        rows = which(dat$id %% 2 != 0)
+      )
+    ) |>
+    opt_table_font("Merriweather") |>
+    ## dencansos
+    tab_style(
+      style     = list(
+        cell_fill(color = "#002500"),
+        cell_text(color = "#e1e3e1")
+      ),
+      locations = cells_body(rows = which(dat$Tipo == "Descanso"))
+    ) |>
+    ## column titles
+    tab_style(
+      style     = list(
+        cell_text(color = "#FFCF99", weight = "bold"),  # Peach title text
+        cell_fill(color = "#8C6239")  # Brown background
+      ),
+      locations = cells_column_labels()
+    ) |>
+    tab_style(
+      style     = list(
+        cell_fill(color = "#002500"),
+        cell_text(color = "#e1e3e1")
+      ),
+      locations = cells_title()
+    ) |>
+    cols_width(
+      Horario ~ px(200)
+    ) |>
+    fmt_missing(missing_text = "")
+}
+
+agenda_table(3) |>
+  gtsave("00_assets/figures/agenda-d1.png")
+
+agenda_table(4, "*Dia 2: Teledetección e Inteligencia Artificial*") |>
+  gtsave("00_assets/figures/agenda-d2.png")
+
+agenda_table(5, "*Dia 3: Teledetección próxima a la Tierra*") |>
+  gtsave("00_assets/figures/agenda-d3.png")
